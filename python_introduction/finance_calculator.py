7ac3b8d4ff6d91f6d9e09c078a5924ed22cfed31
IgharoJoseph <josephigharo@gmail.com>
2025-09-12 05:11:30 +0100

    Updated code for finance calculator to output integer without decimal

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-monthly_income = input("Enter your monthly income:")
-total_monthly_expense = input("Enter your monthly total expense:")
+monthly_income = float(input("Enter your monthly income:"))
+monthly_expense = float(input("Enter your monthly total expense:"))
 
-monthly_savings =  float(monthly_income) - float(total_monthly_expense)
+monthly_savings =  monthly_income - monthly_expense
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}")
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
