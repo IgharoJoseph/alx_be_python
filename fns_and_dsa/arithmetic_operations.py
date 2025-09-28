3889bce6040cacd38a66b2ffcd31e1f6be3c1a60
IgharoJoseph <josephigharo@gmail.com>
2025-09-29 00:54:48 +0100

    I removed typescript

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,8 +11,6 @@ def perform_operation(num1, num2, operation):
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2
-    else:
-        return "Error: Invalid operation"
 
 
 def main():
